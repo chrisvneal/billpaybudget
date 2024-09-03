3f4d318c8bf621ebb6f6640dd46bca1c5761ebef
Chris Neal <chrisvneal@gmail.com>
2024-09-02 16:46:27 -1000

    Add data to template file

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express();
 
 const port = 3000;
 
+let info = {
+	title: "Billpay & Budget",
+	user: "Chris Neal",
+};
+
 app.get("/", (req, res) => {
-	res.render("index.ejs", { title: "Billpay & Budget" });
+	res.render("index.ejs", { info });
 });
 
 app.listen(port, () => {
